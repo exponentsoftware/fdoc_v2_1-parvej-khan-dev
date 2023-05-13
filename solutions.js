574af607c39f8427b98c950365f0bd17ac865390
parvejkhan-dev <kparvej761@gmail.com>
2023-05-13 21:38:45 +0530

    SumOfPrimeNumber

--- solutions.js
+++ solutions.js
@@ -20,12 +20,37 @@ greater than or equal to the input number.
 */
 
 // ["banana", "cherry", "elderberry"]
- const filterByLength = (arr, number) => {
-    let filterdata = arr.filter(el => el.length >= number)
-    return filterdata;
- }
+const filterByLength = (arr, number) => {
+  let filterdata = arr.filter((el) => el.length >= number);
+  return filterdata;
+};
 
- const fruitArray = ["apple", "banana", "cherry", "date", "elderberry"];
+const fruitArray = ["apple", "banana", "cherry", "date", "elderberry"];
 console.log(filterByLength(fruitArray, 6));
 
+/* 
+Write a function called sumOfPrimes that takes a number as 
+input and returns the sum of all prime numbers less than or 
+equal to the input number.
 
+*/
+function sumOfPrimes(number) {
+  /*
+ ? check prime number 
+? if number is prime add on value of sum variable 
+ */
+
+  let sum = 2;
+  let arrSum = [2]
+  for (let i = 2; i <= number; i++) {
+    if (i % 2 !== 0) {
+        arrSum.push(i)
+      sum = sum + i;
+    }
+  }
+  console.log(arrSum, "arrSum")
+return sum;
+}
+
+console.log(sumOfPrimes(13));
+17; // 2 + 3 + 5 + 7 = 17
