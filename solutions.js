8925adf558a67d80977661424c256ce90a46d725
parvejkhan-dev <kparvej761@gmail.com>
2023-05-13 21:16:02 +0530

    solution filterBylength problem

--- solutions.js
+++ solutions.js
@@ -4,9 +4,28 @@
 
  */
 
- const arr = [4, 2, 6, 8, 4, 1, 4];
+const arr = [4, 2, 6, 8, 4, 1, 4];
 console.log(countOccurrences(arr, 4));
-function countOccurrences(arr, number){
-    let filterArr = arr.filter(el => el === number)
-    return filterArr.length
+function countOccurrences(arr, number) {
+  let filterArr = arr.filter((el) => el === number);
+  return filterArr.length;
 }
+
+/* 
+
+1.b. Write a function called filterByLength that 
+takes an array of strings and a number as input
+and returns an array of all the strings in the input array whose length is
+greater than or equal to the input number.
+*/
+
+// ["banana", "cherry", "elderberry"]
+ const filterByLength = (arr, number) => {
+    let filterdata = arr.filter(el => el.length >= number)
+    return filterdata;
+ }
+
+ const fruitArray = ["apple", "banana", "cherry", "date", "elderberry"];
+console.log(filterByLength(fruitArray, 6));
+
+
